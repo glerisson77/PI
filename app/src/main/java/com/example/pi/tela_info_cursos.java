21ba94425d8f6fa8aacd9d09411ce38defdc5b26
Glerisson <glerisson000@gmail.com>
2022-11-08 15:56:30 -0300

    base

---
+++ app/src/main/java/com/example/pi/tela_info_cursos.java
@@ -0,0 +1,14 @@
+package com.example.pi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_info_cursos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_info_cursos);
+    }
+}
