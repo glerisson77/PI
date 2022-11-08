084326fbdf6010157b81ad97c0379f4d22950388
Glerisson <glerisson000@gmail.com>
2022-11-08 15:17:12 -0300

    base

--- app/src/main/java/com/example/pi/MainActivity.java
+++ app/src/main/java/com/example/pi/MainActivity.java
@@ -12,5 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     int i =04455454;
-    String teste = "teste";
+    String teste = "teste2";
 }
