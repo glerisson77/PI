2b4f2dc04d0d62c4080d78879a9edaeb4902ea32
Glerisson <glerisson000@gmail.com>
2022-11-11 16:34:08 -0300

    Icons activity added

---
+++ app/src/main/java/com/example/pi/IconsActivityLayout.java
@@ -0,0 +1,14 @@
+package com.example.pi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IconsActivityLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icons_layout);
+    }
+}
