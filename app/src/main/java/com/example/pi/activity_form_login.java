430e1a5453a191b0de5ce9a43014060b5606d4ea
875816 <cruzfelipe2705@gmail.com>
2022-11-16 15:46:17 -0300

    add 2 telas de cadastro

---
+++ app/src/main/java/com/example/pi/activity_form_login.java
@@ -0,0 +1,13 @@
+package com.example.pi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_form_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
