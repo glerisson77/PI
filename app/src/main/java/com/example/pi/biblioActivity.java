054be24630e193a4db57776d52d0eb18bb4fde82
William Alves da Silva <alveswilliam251@gmail.com>
2022-11-22 16:32:44 -0300

    Willvern

--- app/src/main/java/com/example/pi/biblioActivity.java
+++ app/src/main/java/com/example/pi/biblioActivity.java
@@ -3,12 +3,18 @@ package com.example.pi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class biblioActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biblio);
+        webView.findViewById(R.id.webview);
+        String url = "mg.senac.br/faculdade/Paginas/biblioteca-nova.aspx";
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
     }
 }
--- app/src/main/java/com/example/pi/cursosdisponiveis.java
+++ app/src/main/java/com/example/pi/cursosdisponiveis.java
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-public class mapeamentoActivity extends AppCompatActivity {
+public class cursosdisponiveis extends AppCompatActivity {
     WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mapeamento);
+        setContentView(R.layout.activity_cursosdisponiveis);
         webView.findViewById(R.id.webview);
-        String url = "https://www.google.com/maps/place/Senac/@-21.684343,-45.2637301,20z/data=!4m5!3m4!1s0x94cadd299037c0c1:0x1b4dc77c7d066faa!8m2!3d-21.684343!4d-45.2635";
+        String url = "https://www.mg.senac.br/programasenacdegratuidade/vagas.aspx";
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
