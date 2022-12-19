01a7bdd40dc55f1c799ccee549656cc0ed374a4d
glerisson77 <glerisson000@gmail.com>
2022-12-19 16:58:56 -0300

    19/12/2022

--- app/src/main/java/com/example/pi/ShowFinalScoreActivity.java
+++ app/src/main/java/com/example/pi/ShowFinalScoreActivity.java
@@ -3,25 +3,69 @@ package com.example.pi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class ShowFinalScoreActivity extends AppCompatActivity {
 
-    TextView name, score;
+    TextView name, score, observation;
+    ImageView pontuationBox;
     String passedName, passedScore;
+    LottieAnimationView scoreLottie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_final_score);
-        name = findViewById(R.id.namescore);
+
         score = findViewById(R.id.finalscore);
+        scoreLottie = findViewById(R.id.scorelottie);
+        observation = findViewById(R.id.observationtv);
+        pontuationBox = findViewById(R.id.pontuationbox);
+
 
         passedName = getIntent().getStringExtra("keyname");
         passedScore = getIntent().getStringExtra("keyscore");
 
-        name.setText(passedName);
         score.setText(passedScore);
 
+        addLottieAnimation();
+        setObservationText();
+
+    }
+
+    public void addLottieAnimation(){
+        int intPassedScore = Integer.parseInt(passedScore);
+        if (intPassedScore >= 60){
+            scoreLottie.setAnimation(R.raw.businessmanwinnerwithtrophy);
+        }else if (intPassedScore >= 30){
+            scoreLottie.setAnimation(R.raw.businessmanwinnerwithtrophy);
+        }else if (intPassedScore <= 29){
+            scoreLottie.setAnimation(R.raw.businessmanwinnerwithtrophy);
+        }
+    }
+
+    public void setObservationText(){
+        int intPassedScore = Integer.parseInt(passedScore);
+        if (intPassedScore >= 60){
+            observation.setText("Pefeito!");
+        }else if (intPassedScore >= 30){
+            observation.setText("Estude mais!");
+        }else if (intPassedScore <= 29){
+            observation.setText("Que pena!!!");
+        }
+    }
+
+    public void setBoxColor(){
+        int intPassedScore = Integer.parseInt(passedScore);
+        if (intPassedScore >= 60){
+           pontuationBox.setImageResource(R.drawable.greenbox);
+        }else if (intPassedScore >= 30){
+            pontuationBox.setImageResource(R.drawable.greenbox);
+        }else if (intPassedScore <= 29){
+            pontuationBox.setImageResource(R.drawable.greenbox);
+        }
     }
 }
